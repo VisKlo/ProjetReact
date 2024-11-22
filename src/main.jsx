c3fd37cd3fbdd28f21e07bf9f83ff28690158fc3
Clovis <jocailleclovis59@mgmail.com>
2024-11-22 13:04:58 +0100

    e

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
-import './reset.css'
-import './style.css'
+import './assets/reset.css'
+import './assets/style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
